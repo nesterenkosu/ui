d150877526d127f0825a0378c42e95ea46c2a200
nesterenkosu <62102496+nesterenkosu@users.noreply.github.com>
2020-04-23 18:05:17 +0500

    Add files via upload

---
+++ Code/Gtk#/AppsFromMSWindows/2_0_Console/HelloWin/HelloWin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from windows and .NET Framework 2.0 !");
+            Console.ReadLine();
+        }
+    }
+}
